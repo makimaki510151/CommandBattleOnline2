25e4e49a5b40cbdea9290fbbd8895334c47a3722
makimaki510151 <rsv95076@gmail.com>
2025-09-23 01:27:42 +0900

    どうだ！

--- api/token.js
+++ api/token.js
@@ -2,12 +2,12 @@
 
 // SkyWay SDK v3のライブラリをインポート
 import { SkyWayAuthToken } from '@skyway-sdk/token';
-import * as common from '@skyway-sdk/common';
 
-export default (req, res) => {
+// Vercelに設定した環境変数を読み込む
+export default async (req, res) => {
     console.log('token.js: Function started.');
 
-    // Vercelに設定した環境変数を関数内で読み込む
+    // 環境変数を関数内で読み込む
     const APP_ID = process.env.SKYWAY_APP_ID;
     const SECRET = process.env.SKYWAY_SECRET_KEY;
 
@@ -22,11 +22,17 @@ export default (req, res) => {
     }
 
     try {
+        console.log('token.js: Attempting to dynamically import @skyway-sdk/common...');
+        // @skyway-sdk/common を動的にインポート
+        const common = await import('@skyway-sdk/common');
+        const { uuidV4, nowInSec } = common;
+
+        console.log('token.js: Dynamically imported @skyway-sdk/common successfully.');
         console.log('token.js: Attempting to create SkyWayAuthToken...');
         // トークンを生成
         const token = new SkyWayAuthToken({
-            jti: common.uuidV4(),
-            iat: common.nowInSec(),
+            jti: uuidV4(),
+            iat: nowInSec(),
             exp: nowInSec() + 60 * 60 * 2, // 2時間
             scope: {
                 appId: APP_ID,
